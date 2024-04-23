ddd4e4abb429354a38dd6ce138d222919f0e7c7a
Julien Lach <leadedchamps@gmail.com>
2024-04-23 15:30:44 +0200

    JS async sandbox

---
+++ async_await_fetch.js
@@ -0,0 +1,9 @@
+async function fetchUsers() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+
+  const data = await response.json();
+
+  console.log(data);
+}
+
+fetchUsers();
